24e34a44a89ffd7570daec451af469a1b9a6d33d
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-06-01 08:08:52 +0530

    sec

--- frontedns/src/utils/ProtectRoutes.jsx
+++ frontedns/src/utils/ProtectRoutes.jsx
@@ -7,11 +7,11 @@ const ProtectRoutes = ({children
     // ,requiredRole
 }) => {
   // const role='admin'
-    // const token=useSelector((state)=>state.user.token) 
+    const token=useSelector((state)=>state.user.token) 
     // const user=useSelector((state)=>state.user.user)
-    // if(!token){
-    //   return  <Navigate to='/login' replace />
-    // }
+    if(!token){
+      return  <Navigate to='/login' replace />
+    }
     // if(requiredRole && requiredRole!==role){
     //   console.log("hello")
     //    return <Navigate to='/register' replace />
